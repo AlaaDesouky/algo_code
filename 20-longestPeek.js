7aa2d06921894fe6fc092aa7f908fee583441104
Desouky <alaa.desouky33@gmail.com>
2022-05-06 01:13:20 +0200

    longestPeek

---
+++ 20-longestPeek.js
@@ -0,0 +1,34 @@
+// Objectives: given an array, find the longest peek tracking a sequence of numbers
+// Data: array = [1,2,3,3,4,0,10,6,5,-1,-3,2,3]
+// Method: traverse the array, check if the current value represents a peak, if so calculate its distance 
+
+// Approach: O(n) time | O(1) space
+function longestPeek(array) {
+  let longestPeekLength = 0
+  let i = 1
+  while (i < array.length - 1) {
+    let isPeak = array[i - 1] < array[i] && array[i] > array[i + 1]
+    if (!isPeak) {
+      i++
+      continue
+    }
+
+    let leftIdx = i - 2
+    let rightIdx = i + 2
+    while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]) {
+      leftIdx--
+    }
+    while (rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]) {
+      rightIdx++
+    }
+
+    let currentPeakLength = rightIdx - leftIdx - 1
+    longestPeekLength = Math.max(longestPeekLength, currentPeakLength)
+    i = rightIdx
+  }
+  return longestPeekLength
+}
+
+// Test
+let testArray = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+console.log(longestPeek(testArray)) // 6
